00101b9bc127bcb3c4a538cf7cc404f08017fe2c
William Dwyer <wbdwyer883@gmail.com>
2024-01-13 16:06:22 -0600

    Initial commit

---
+++ backend/lumber-logic/src/main/java/com/lumberlogic/web/LumberLogicApplication.java
@@ -0,0 +1,13 @@
+package com.lumberlogic.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumberLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumberLogicApplication.class, args);
+	}
+
+}
